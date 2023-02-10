90b6c2aa938ca40a68ee245d1027b1014b044f3e
Adi131313 <110452111+Adi131313@users.noreply.github.com>
2023-02-10 12:21:00 +0200

    Update test.R

--- tests/experimental/test.R
+++ tests/experimental/test.R
@@ -1,5 +1,3 @@
-
-
 n = 10
 R = c(3, 5)
 tmp = duct(n = n, center = c(1,3))
@@ -8,5 +6,15 @@ lines(tmp)
 tmp = duct(n = n, R = R)
 lines(tmp)
 
+
+n = c(20, 15)
+plot.base(xlim=c(-20,20), ylim=c(-20,20))
+lines(duct(n[1], c(15,18), phi=pi/2/n[1], fill = "#0048C0"))
+lines(duct(n[1], c(9,13), phi=c(0, pi/n[2]), nc.r = NULL,
+           fill = "#8080F0"), lwd = 5)
+lines(duct(n[1], c(5,8)))
+abline(h=0, col="green")
+
+warnings()
 
 dev.off()
