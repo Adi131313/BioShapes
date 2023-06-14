02061be2427a5ad57c97a21b9e941877225ff9d4
Adi131313 <adicotoc@yahoo.com>
2023-06-14 19:02:17 +0300

    Clean examples

--- tests/experimental/test.R
+++ tests/experimental/test.R
@@ -1,6 +1,6 @@
 # TODO: create complex examples in R/Examples.R
 
-#TODO: colors for spikes
+# TODO: colors for spikes
 # Virus
 N = 10
 R = 3
@@ -50,103 +50,12 @@ xy = helix.rad(R = R, n=n, phi=pi, r = 2);
 lines(xy, col="red")
 
 
-###TODO 1: Dendrite
-###TODO 2: ADN
+### TODO 1: Dendrite as tree
+### TODO 2: ADN
 
-# TODO: function for muscle
-### muscle tissue ###
-scale.x = 1.5
-scale.R = c(1.5, 1.5)
-x = c(-2, 2); y = c(1, 1);
-dy = 0.4 # 0.36
-dx = 2
-mx = (x[1] + x[2])/2;
-my = (y[1] + y[2])/2;
-n = 6 # number of layers
-fill = "red";
+# TODO: Centers for muscles
 
-lst = list()
-for(iy in seq(0,n)){
-  if(iy%%2 == 0){
-    for(idx in seq(-2,2,2)){
-      lens = lens(x = x + idx*dx, y = y + iy*dy, scale=scale.R)
-      lst = c(lst, lens)
-    }}
-  else{
-    for(idx in seq(-3,3,2)){
-      lens = lens(x = x + idx*dx, y = y + iy*dy, scale=scale.R)
-      lst = c(lst, lens)
-    }
-  }}
-lst = as.bioshape(lst)
-plot.base()
-lines(lst, fill=fill)
-
-
-# #2nd level
-# lens = lens(x = x + dx, y = y + dy, scale=scale.R)
-# lines(lens, fill=fill)
-# lens = lens(x = x + 3*dx, y = y + dy, scale=scale.R)
-# lines(lens, fill=fill)
-# lens = lens(x = x - dx, y = y + dy, scale=scale.R)
-# lines(lens, fill=fill)
-# lens = lens(x = x - 3*dx, y = y + dy, scale=scale.R)
-# lines(lens, fill=fill)
-# #3rd level
-# lens = lens(x = x, y = y + 2*dy, scale=scale.R)
-# lines(lens, fill=fill)
-# lens = lens(x = x + 2*dx, y = y + 2*dy, scale=scale.R)
-# lines(lens, fill=fill)
-# lens = lens(x = x - 2*dx, y = y + 2*dy, scale=scale.R)
-# lines(lens, fill=fill)
-# #4th level
-# lens = lens(x = x + dx, y = y + 3*dy, scale=scale.R)
-# lines(lens, fill=fill)
-# lens = lens(x = x + 3*dx, y = y + 3*dy, scale=scale.R)
-# lines(lens, fill=fill)
-# lens = lens(x = x - dx, y = y + 3*dy, scale=scale.R)
-# lines(lens, fill=fill)
-# lens = lens(x = x - 3*dx, y = y + 3*dy, scale=scale.R)
-# lines(lens, fill=fill)
-# #5th level
-# lens = lens(x = x, y = y + 4*dy, scale=scale.R)
-# lines(lens, fill=fill)
-# lens = lens(x = x + 2*dx, y = y + 4*dy, scale=scale.R)
-# lines(lens, fill=fill)
-# lens = lens(x = x - 2*dx, y = y + 4*dy, scale=scale.R)
-# lines(lens, fill=fill)
-# #6th level
-# lens = lens(x = x + dx, y = y + 5*dy, scale=scale.R)
-# lines(lens, fill=fill)
-# lens = lens(x = x + 3*dx, y = y + 5*dy, scale=scale.R)
-# lines(lens, fill=fill)
-# lens = lens(x = x - dx, y = y + 5*dy, scale=scale.R)
-# lines(lens, fill=fill)
-# lens = lens(x = x - 3*dx, y = y + 5*dy, scale=scale.R)
-# lines(lens, fill=fill)
-# #7th level
-# lens = lens(x = x, y = y + 6*dy, scale=scale.R)
-# lines(lens, fill=fill)
-# lens = lens(x = x + 2*dx, y = y + 6*dy, scale=scale.R)
-# lines(lens, fill=fill)
-# lens = lens(x = x - 2*dx, y = y + 6*dy, scale=scale.R)
-# lines(lens, fill=fill)
-
-# Centers for lens
-lst = list(center = c(mx, my), r = 0.15, fill = "yellow")
-class(lst) = c("circle", "list");
-lst = as.bioshape(list(lst));
-lines(lst)
-lst = list(center = c(mx + 2*dx, my), r = 0.15, fill = "yellow")
-class(lst) = c("circle", "list");
-lst = as.bioshape(list(lst));
-lines(lst)
-lst = list(center = c(mx - 2*dx, my), r = 0.15, fill = "yellow")
-class(lst) = c("circle", "list");
-lst = as.bioshape(list(lst));
-lines(lst)
-
-# dna.new
+# DNA
 
 h2 = dna.new(c(1,5), c(1,6), phi = c(0, pi))
 plot.base()
@@ -154,7 +63,6 @@ lines(h2)
 
 #test function
 # TODO: Also one for Virus
-# TODO: Rename all examples to 'example'
 description.neuron()
 
 
@@ -195,7 +103,7 @@ tmp = neuron(c(0,2), phi=pi/6)
 lines(tmp)
 lines(synapse(c(tmp[[6]]$x[2], tmp[[6]]$y[2]), slope=tan(pi/6), type="Tree", l=1.8, alpha=120))
 
-
+# TODO: example of design for DNA
 # Intersection of sine functions
 phi = c(0.7, 1.5)
 curve(sin(x + phi[1]*pi), -2*pi, 6*pi)
